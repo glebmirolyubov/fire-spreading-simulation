daad61974884fb554a87335c952a85e9c98c2046
Gleb <gleb.mirolyubov@gmail.com>
2019-07-08 15:52:54 +0100

    Added wind and plant prefab

---
+++ Fire Spreading Simulation/Assets/Scripts/WindManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WindManager : MonoBehaviour
+{
+    [SerializeField]
+    private WindZone wind;
+    [SerializeField]
+    private Text windStrengthText;
+    [SerializeField]
+    private Text windDirectionText;
+
+    float minWindStrength = 0f;
+    float maxWindStrength = 100f;
+
+    float minWindDirection = 0f;
+    float maxWindDirection = 360f;
+
+    public void ChangeWindStrength (float strength)
+    {
+        wind.windMain = strength;
+
+        windStrengthText.text = strength.ToString();
+    }
+
+    public void ChangeWindDirection (float direction)
+    {
+        wind.gameObject.transform.rotation = Quaternion.Euler(0, direction, 0);
+
+        windDirectionText.text = direction.ToString();
+    }
+}
